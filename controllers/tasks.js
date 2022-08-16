c4f894319654635c5dd09e259a0a60b0e5397be1
jing <jingli1524@gmail.com>
2022-08-15 23:17:44 -0400

    more controller functions

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,20 +1,54 @@
-const getAllTasks = (req, res) => {
-  res.send("get all tasks lalalalalallalalal");
-};
+const Task = require("../models/task");
 
-const createTasks = (req, res) => {
-  res.json(req.body);
+const getAllTasks = async (req, res) => {
+  try {
+    const allTasks = await Task.find({}); //empty obj get all of the data
+    res.status(200).json({ allTasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
-
-const getTask = (req, res) => {
-  res.json(req.params.id);
+// ================================================
+const createTasks = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
+// ================================================
 
-const updateTasks = (req, res) => {
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+// ================================================
+
+const updateTasks = async (req, res) => {
   res.send("update the task wahhhhhhhhhhhhhhh");
 };
-const deleteTasks = (req, res) => {
-  res.send("delete a task ahhhhhhhhhhhhhhh");
+// ================================================
+
+const deleteTasks = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
